8db2d11d5d7ac0c0d7ab12b706bde46e42d862d6
SaakshiHegde <saakshi.hegde23@gmail.com>
2021-04-30 11:56:52 +1200

    New printGrid method

--- Sudoku/src/Game.java
+++ Sudoku/src/Game.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner; //Import the Scanner class
 
 public class Game {
@@ -7,6 +8,12 @@ public class Game {
 	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
+		int rows = 9;
+		int cols = 9;
+		int i,j;
+		int incorrect = 0;
+		
 		int [] [] grid = { 
 				{6, 0, 0, 8, 0, 0, 3, 0, 0},
 				{5, 0, 0, 0, 2, 0, 0, 0, 0},
@@ -17,16 +24,12 @@ public class Game {
 				{3, 0, 9, 5, 0, 8, 4, 0, 0},
 				{7, 8, 0, 0, 4, 2, 9, 1, 0},
 				{0, 0, 2, 0, 0, 0, 0, 8, 0} };
-		int [] [] grid2 = { 
-				{6, 0, 0, 8, 0, 0, 3, 0, 0},
-				{5, 0, 0, 0, 2, 0, 0, 0, 0},
-				{1, 0, 0, 4, 0, 3, 0, 5, 6},
-				{0, 6, 1, 0, 0, 0, 5, 9, 8},
-				{9, 0, 4, 6, 0, 5, 0, 0, 2},
-				{8, 3, 5, 0, 0, 9, 7, 0, 0},
-				{3, 0, 9, 5, 0, 8, 4, 0, 0},
-				{7, 8, 0, 0, 4, 2, 9, 1, 0},
-				{0, 0, 2, 0, 0, 0, 0, 8, 0} };
+		int [] [] grid2 = new int [9][9];
+		
+		for (i = 0; i < rows; i++) {
+			grid2[i] = Arrays.copyOf(grid[i], 9);
+		}
+		
 		int [] [] answer = { 
 				{6, 2, 7, 8, 5, 1, 3, 4, 9},
 				{5, 4, 3, 9, 2, 6, 8, 7, 1},
@@ -37,6 +40,7 @@ public class Game {
 				{3, 1, 9, 5, 6, 8, 4, 2, 7},
 				{7, 8, 6, 3, 4, 2, 9, 1, 5},
 				{4, 5, 2, 1, 9, 7, 6, 8, 3} };
+		int [][] miniGrid0 = new int [3][3];
 		int [][] miniGrid1 = new int [3][3];
 		int [][] miniGrid2 = new int [3][3];
 		int [][] miniGrid3 = new int [3][3];
@@ -45,66 +49,35 @@ public class Game {
 		int [][] miniGrid6 = new int [3][3];
 		int [][] miniGrid7 = new int [3][3];
 		int [][] miniGrid8 = new int [3][3];
-		int [][] miniGrid9 = new int [3][3];
 		
-		int rows = 9;
-		int cols = 9;
-		int i,j;
-		int incorrect = 0;
-		for (i=0; i<3; i++) {
-			for (j=0; j<3; j++) {
-				miniGrid1 [i][j] = grid[i][j];
-			}
-		}
-		for (i=0; i<3; i++) {
-			for (j=3; j<6; j++) {
-				miniGrid2 [i][j-3] = grid[i][j];
-			}
-		}
-		for (i=0; i<3; i++) {
-			for (j=6; j<9; j++) {
-				miniGrid3 [i][j-6] = grid[i][j];
-			}
-		}
-		for (i=3; i<6; i++) {
-			for (j=0; j<3; j++) {
-				miniGrid4 [i-3][j] = grid[i][j];
-			}
-		}
-		for (i=3; i<6; i++) {
-			for (j=3; j<6; j++) {
-				miniGrid5 [i-3][j-3] = grid[i][j];
-			}
-		}
-		for (i=3; i<6; i++) {
-			for (j=6; j<9; j++) {
-				miniGrid6 [i-3][j-6] = grid[i][j];
-			}
-		}
-		for (i=6; i<9; i++) {
-			for (j=0; j<3; j++) {
-				miniGrid7 [i-6][j] = grid[i][j];
-			}
-		}
-		for (i=6; i<9; i++) {
-			for (j=3; j<6; j++) {
-				miniGrid8 [i-6][j-3] = grid[i][j];
-			}
-		}
-		for (i=6; i<9; i++) {
-			for (j=6; j<9; j++) {
-				miniGrid9 [i-6][j-6] = grid[i][j];
+		
+		for (i=0; i<9; i++) {
+			for (j=0; j<9; j++) {
+				if (i<3 && j<3) {
+					miniGrid0 [i][j] = grid[i][j];
+				} else if (i<3 && j>=3 && j<6) {
+					miniGrid1 [i][j-3] = grid[i][j];
+				} else if (i<3 && j>=6) {
+					miniGrid2 [i][j-6] = grid[i][j];
+				} else if (i>=3 && i<6 && j<3) {
+					miniGrid3 [i-3][j] = grid[i][j];
+				} else if (i>=3 && i<6 && j>=3 && j<6) {
+					miniGrid4 [i-3][j-3] = grid[i][j];
+				} else if (i>= 3 && i<6 && j>=6 && j<9) {
+					miniGrid5 [i-3][j-6] = grid[i][j];
+				} else if (i>=6 && i<9 && j<3) {
+					miniGrid6 [i-6][j] = grid[i][j];
+				} else if (i>=6 && i<9 && j>=3 && j<6) {
+					miniGrid7 [i-6][j-3] = grid[i][j];
+				} else {
+					miniGrid8 [i-6][j-6] = grid[i][j];
+				}
 			}
 		}
 		
 		while (grid != answer) {
 
-			for (i = 0; i < rows; i++) {
-				for (j = 0; j < cols; j++) {
-					System.out.print(grid[i][j] + " ");
-				}
-				System.out.println(" ");
-			}
+			printGrid(grid);
 			incorrect = 0;
 			System.out.println("Where do you want to enter number? Row:");
 			int r = input.nextInt();
@@ -127,79 +100,85 @@ public class Game {
 						break;
 					}
 				}
-				if (r<3 && r>=0 && c<3 && c>=0) {
-					for (i=0; i<3; i++) {
-						for (j=0; j<3; j++) {
-							if (miniGrid1[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				} else if (r<3 && r>=0 && c<6 && c>=3) {
-					for (i=0; i<3; i++) {
-						for (j=3; j<6; j++) {
-							if (miniGrid2[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				}else if (r<3 && r>=0 && c<9 && c>=6) {
-					for (i=0; i<3; i++) {
-						for (j=6; j<9; j++) {
-							if (miniGrid3[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				}else if (r<6 && r>=3 && c<3 && c>=0) {
-					for (i=3; i<6; i++) {
-						for (j=0; j<3; j++) {
-							if (miniGrid4[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				} else if (r<6 && r>=3 && c<6 && c>=3) {
-					for (i=3; i<6; i++) {
-						for (j=3; j<6; j++) {
-							if (miniGrid5[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				}else if (r<6 && r>=3 && c<9 && c>=6) {
-					for (i=3; i<6; i++) {
-						for (j=6; j<9; j++) {
-							if (miniGrid6[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				} else if (r<9 && r>=6 && c<3 && c>=0) {
-					for (i=6; i<9; i++) {
-						for (j=0; j<3; j++) {
-							if (miniGrid7[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				} else if (r<9 && r>=6 && c<3 && c>=0) {
-					for (i=6; i<9; i++) {
-						for (j=3; j<6; j++) {
-							if (miniGrid8[i][j] == number) {
-								incorrect = 1;
-							}
-						}
-					}
-				} else if (r<9 && r>=6 && c<3 && c>=0) {
-					for (i=6; i<9; i++) {
-						for (j=6; j<9; j++) {
-							if (miniGrid9[i][j] == number) {
-								incorrect = 1;
-							}
-						}
+				int Index = c/3 + 3*(r/3);
+				for (i=0; i<3; i++) {
+					for (j=0; j<3; j++) {
+						
 					}
 				}
+//				if (r<3 && r>=0 && c<3 && c>=0) {
+//					for (i=0; i<3; i++) {
+//						for (j=0; j<3; j++) {
+//							if (miniGrid1[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				} else if (r<3 && r>=0 && c<6 && c>=3) {
+//					for (i=0; i<3; i++) {
+//						for (j=3; j<6; j++) {
+//							if (miniGrid2[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				}else if (r<3 && r>=0 && c<9 && c>=6) {
+//					for (i=0; i<3; i++) {
+//						for (j=6; j<9; j++) {
+//							if (miniGrid3[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				}else if (r<6 && r>=3 && c<3 && c>=0) {
+//					for (i=3; i<6; i++) {
+//						for (j=0; j<3; j++) {
+//							if (miniGrid4[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				} else if (r<6 && r>=3 && c<6 && c>=3) {
+//					for (i=3; i<6; i++) {
+//						for (j=3; j<6; j++) {
+//							if (miniGrid5[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				}else if (r<6 && r>=3 && c<9 && c>=6) {
+//					for (i=3; i<6; i++) {
+//						for (j=6; j<9; j++) {
+//							if (miniGrid6[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				} else if (r<9 && r>=6 && c<3 && c>=0) {
+//					for (i=6; i<9; i++) {
+//						for (j=0; j<3; j++) {
+//							if (miniGrid7[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				} else if (r<9 && r>=6 && c<3 && c>=0) {
+//					for (i=6; i<9; i++) {
+//						for (j=3; j<6; j++) {
+//							if (miniGrid8[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				} else if (r<9 && r>=6 && c<3 && c>=0) {
+//					for (i=6; i<9; i++) {
+//						for (j=6; j<9; j++) {
+//							if (miniGrid9[i][j] == number) {
+//								incorrect = 1;
+//							}
+//						}
+//					}
+//				}
 				
 				if (incorrect == 1) {
 					System.out.println("Incorrect move. Try again!");
@@ -213,4 +192,20 @@ public class Game {
 		}
 		System.out.println("You win!!");
 	}
+	
+	private static void printGrid (int [][] grid) {
+		int rows = grid.length;
+		int cols = grid[0].length;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if (grid[i][j] == 0) {
+					System.out.print("# ");
+				} else {
+					System.out.print(grid[i][j] + " ");
+				}
+			}
+			System.out.println(" ");
+		}		
+	}
 }
+
